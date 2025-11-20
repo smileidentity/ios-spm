43f3f40bd8e354303cc0345c722203275551d09c
Tobi Omotayo <tobitech@ymail.com>
2025-11-20 13:06:58 +0100

    formatting.

--- Package.swift
+++ Package.swift
@@ -4,37 +4,37 @@
 import PackageDescription
 
 let package = Package(
-	name: "UseSmileID",
-	platforms: [.iOS(.v15)],
-	products: [
-		.library(
-			name: "UseSmileID",
-			targets: ["UseSmileID", "SmileIDML"]
-		),
-		.library(
-			name: "SmileIDML",
-			targets: ["SmileIDML"]
-		),
-		.library(
-			name: "SmileIDMLMediapipe",
-			targets: ["SmileIDMLMediapipe"]
-		)
-	],
-	targets: [
-		.binaryTarget(
-			name: "UseSmileID",
-			url: "https://github.com/smileidentity/ios-demo/releases/download/v1.0.0/UseSmileID.xcframework.zip",
-			checksum: "49d6352f715a2ce6421775e602e349109740585efd8b67eaffdf6a1d7275018a"
-		),
-		.binaryTarget(
-			name: "SmileIDML",
-			url: "https://github.com/smileidentity/ios-demo/releases/download/v1.0.0/SmileIDML.xcframework.zip",
-			checksum: "dda07de80cf6d0e0330e428fafed8cbf7ed9cf3969d19b6ee055f6fcd79d7d89"
-		),
-		.binaryTarget(
-			name: "SmileIDMLMediapipe",
-			url: "https://github.com/smileidentity/ios-demo/releases/download/v1.0.0/SmileIDMLMediapipe.xcframework.zip",
-			checksum: "4d3fbc9ec330f7251ccab5dc12191219285872ccf530b9735e642b39df8dff02"
-		)
-	]
+    name: "UseSmileID",
+    platforms: [.iOS(.v15)],
+    products: [
+        .library(
+            name: "UseSmileID",
+            targets: ["UseSmileID", "SmileIDML"]
+        ),
+        .library(
+            name: "SmileIDML",
+            targets: ["SmileIDML"]
+        ),
+        .library(
+            name: "SmileIDMLMediapipe",
+            targets: ["SmileIDMLMediapipe"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "UseSmileID",
+            url: "https://github.com/smileidentity/ios-demo/releases/download/v1.0.0/UseSmileID.xcframework.zip",
+            checksum: "49d6352f715a2ce6421775e602e349109740585efd8b67eaffdf6a1d7275018a"
+        ),
+        .binaryTarget(
+            name: "SmileIDML",
+            url: "https://github.com/smileidentity/ios-demo/releases/download/v1.0.0/SmileIDML.xcframework.zip",
+            checksum: "dda07de80cf6d0e0330e428fafed8cbf7ed9cf3969d19b6ee055f6fcd79d7d89"
+        ),
+        .binaryTarget(
+            name: "SmileIDMLMediapipe",
+            url: "https://github.com/smileidentity/ios-demo/releases/download/v1.0.0/SmileIDMLMediapipe.xcframework.zip",
+            checksum: "4d3fbc9ec330f7251ccab5dc12191219285872ccf530b9735e642b39df8dff02"
+        ),
+    ]
 )
--- Tests/ios-demoTests/ios_demoTests.swift
+++ Tests/ios-demoTests/ios_demoTests.swift
@@ -1,5 +1,5 @@
-import Testing
 @testable import ios_demo
+import Testing
 
 @Test func example() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
